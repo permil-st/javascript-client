0f2cb43339227616ff54633b896e301de5bf152c
permil-st <permil.garg@successive.tech>
2020-03-03 15:16:40 +0530

    style: change as requested in PR #5
    remove dead code, remove husky object from package.json, move css from inlined to styled-component

--- src/components/TextField/TextField.jsx
+++ src/components/TextField/TextField.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import TextFieldStyle from './style';
 
-// <input type="text" className="TextField" value={value} disabled={(disabled)} />
 const TextField = ({ value = '', disabled = false, error }) => (
   <div>
     <TextFieldStyle type="text" className={(error) ? 'error' : ''} value={value} disabled={(disabled)} />
@@ -15,22 +14,5 @@ TextField.propTypes = {
   disabled: PropTypes.bool.isRequired,
   error: PropTypes.string.isRequired,
 };
-
-// class TextField extends React.Component {
-
-//   constructor(props) {
-//     super(props);
-//   }
-
-//   render() {
-
-//     const { value='', disabled = false, error } = this.props;
-
-//     return (
-//       <input type="text" value={value} disabled = { (disabled) }/>
-//     )
-//   }
-
-// }
 
 export default TextField;
--- src/pages/TextFieldDemo/TextFieldDemo.jsx
+++ src/pages/TextFieldDemo/TextFieldDemo.jsx
@@ -1,23 +1,22 @@
 import React from 'react';
 import { TextField } from '../../components';
-
-const style = { 'font-weight': 'bold' };
+import { Label, Wrapper, Item } from './style';
 
 const TextFieldDemo = () => (
-  <div style={{ margin: '16px' }}>
-    <div style={{ padding: '16px 0px' }}>
-      <h4 style={style}>This is Disabled Input</h4>
+  <Wrapper>
+    <Item>
+      <Label>This is Disabled Input</Label>
       <TextField value="Disabled Input" disabled />
-    </div>
-    <div style={{ padding: '16px 0px' }}>
-      <h4 style={style}>A Valid Input</h4>
+    </Item>
+    <Item>
+      <Label>A Valid Input</Label>
       <TextField value="Accessible" />
-    </div>
-    <div style={{ padding: '16px 0px' }}>
-      <h4 style={style}>An Input with errors</h4>
+    </Item>
+    <Item>
+      <Label>An Input with errors</Label>
       <TextField value="101" error="Could not be greater than 100" />
-    </div>
-  </div>
+    </Item>
+  </Wrapper>
 );
 
 export default TextFieldDemo;
