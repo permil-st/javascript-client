45d99bf0aed20271f40fe28444c954b9ade68573
permil <garg.permil@gmail.com>
2020-03-25 12:05:34 +0530

    test: 💍 change test case

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/[*]/i);
+  const linkElement = getByText(/TRAINEE PORTAL/i);
   expect(linkElement).toBeInTheDocument();
 });
