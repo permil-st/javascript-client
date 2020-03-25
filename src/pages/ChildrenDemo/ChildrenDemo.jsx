4ce6b76fffded88f8a98477087aad9cfb5681b2e
permil <garg.permil@gmail.com>
2020-03-25 11:49:34 +0530

    refactor: 💡 change as requested in PR#9
    ✅ Closes: #40791

--- src/pages/ChildrenDemo/ChildrenDemo.jsx
+++ src/pages/ChildrenDemo/ChildrenDemo.jsx
@@ -7,6 +7,7 @@ const ChildrenDemo = () => (
   <>
     <Math first={1} second={2} operator="+" />
     <Math first={4} second={20} operator="*" />
+    <Math first={4} second={20} operator="**" />
     <Math first={8} second={0} operator="/" />
     <Math first={16} second={52} operator="+">
       {
